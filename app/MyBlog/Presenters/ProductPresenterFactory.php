ebc0baac6803a8dc92ba7d3d07c0097747d3cae5
oomusou <oomusou@gmail.com>
2015-12-08 19:17:37 +0800

    1. ProductPresenterFactory改成bindProductPresenter() 2. 由App::bind()改成$this->app->bind()

--- app/MyBlog/Presenters/ProductPresenterFactory.php
+++ app/MyBlog/Presenters/ProductPresenterFactory.php
@@ -1,13 +1,23 @@
 <?php
 
-
 namespace MyBlog\Presenters;
 
-
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Facades\App;
 
 class ProductPresenterFactory
 {
+    /** @var Application */
+    private $app;
+
+    /**
+     * ProductPresenterFactory constructor.
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
 
     /**
      * @param string $lang
@@ -15,12 +25,11 @@ class ProductPresenterFactory
     public function bindProductPresenter($lang)
     {
         if ($lang == 'zh-tw') {
-            App::bind(MoneyFormatInterface::class, ProductPresenterZHTW::class);
+            $this->app->bind(MoneyFormatInterface::class, ProductPresenterZHTW::class);
         } elseif ($lang == 'zh-cn') {
-            App::bind(MoneyFormatInterface::class, ProductPresenterZHCN::class);
+            $this->app->bind(MoneyFormatInterface::class, ProductPresenterZHCN::class);
         } else {
-            App::bind(MoneyFormatInterface::class, ProductPresenterDefault::class);
+            $this->app->bind(MoneyFormatInterface::class, ProductPresenterDefault::class);
         }
     }
-
 }
